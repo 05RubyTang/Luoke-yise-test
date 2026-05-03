57561c1ba3d05a05d8751f1947255174f6113cf9
05RubyTang <2429125131@qq.com>
2026-05-03 19:27:20 +0800

    feat: 更新公告icon换成公告.png图片

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -954,7 +954,7 @@ export default function Profile({ navigate }) {
               }}
             >
               <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-                <span style={{ fontSize: 16 }}>📢</span>
+                <img src={`${import.meta.env.BASE_URL}announcement-icon.png`} alt="" style={{ width: 20, height: 'auto' }} />
                 <span style={{ fontSize: 13, fontWeight: 800, color: '#2B2A2E', fontFamily: 'var(--font-display)' }}>
                   更新公告
                 </span>
